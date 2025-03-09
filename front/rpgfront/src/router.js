43073a7b04f54ae48d16b078466488e554aacc74
GusT177 <gustavocarmanhanis@gmail.com>
2025-03-08 22:08:05 -0300

    conflict_solve

--- front/rpgfront/src/router.js
+++ front/rpgfront/src/router.js
@@ -3,13 +3,13 @@ import { createRouter, createMemoryHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import Login from './pages/Login.vue'
 import Register from './pages/Register.vue'
-import Campaign from './pages/Campaign.vue';
+import Campaign from './pages/Campaign.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
-    { path: '/campaign', component: Campaign}
+    { path: '/campaign', component: Campaign},
 ]
 
 const router = createRouter({
